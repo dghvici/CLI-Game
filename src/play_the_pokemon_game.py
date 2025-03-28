f4e77835a4b633e0da6ff6cfdbd45e46608f9494
dghvici <daniela.ghinchevici@gmail.com>
2025-03-28 15:16:20 +0000

    flake8 complience checked

--- src/play_the_pokemon_game.py
+++ src/play_the_pokemon_game.py
@@ -1,78 +1,166 @@
 import random
-from pokemon import Pokemon, Fire, Water, Grass, Normal, Pokeball, Trainer, Battle
+from pokemon import (
+    Fire,
+    Water,
+    Grass,
+    Normal,
+    Trainer,
+    Battle,
+)
 
-pokemon_dict = {Fire: ['Charmander', 44, 17, 'Flamethrower','fire', 'grass', 'water'],
-    Water: ['Vaporeon', 70, 19, 'Hydro pump', 'water', 'fire', 'grass'],
-    Grass: ['Leafeon', 65, 17, 'Giga drain', 'grass', 'water', 'fire'],
-    Normal: ['Eevee', 55, 18, 'Headbutt', 'normal', None, 'Fighting'],
-    Fire: ['Flareon', 65, 20, 'Fire blast', 'fire', 'grass','Water'],
-    Water: ['Squirtle', 44, 16, 'Surf', 'water', 'fire', 'grass'],
-    Grass: ['Bulbasaur', 45, 16, 'Razor leaf', 'grass', 'water', 'fire']}
+pokemon_dict = {
+    Fire: ["Charmander", 44, 17, "Flamethrower", "fire", "grass", "water"],
+    Water: ["Vaporeon", 70, 19, "Hydro pump", "water", "fire", "grass"],
+    Grass: ["Leafeon", 65, 17, "Giga drain", "grass", "water", "fire"],
+    Normal: ["Eevee", 55, 18, "Headbutt", "normal", None, "Fighting"],
+    Fire: ["Flareon", 65, 20, "Fire blast", "fire", "grass", "Water"],
+    Water: ["Squirtle", 44, 16, "Surf", "water", "fire", "grass"],
+    Grass: ["Bulbasaur", 45, 16, "Razor leaf", "grass", "water", "fire"],
+}
 
 
-### PLAYER 1 - POKEMON 1 ###
+# PLAYER 1 - POKEMON 1 #
 
-print('Please enter the name of player_1.')
+print("Please enter the name of player_1.")
 player_1_name = input()
-print(f'Hey {player_1_name}! Welcome to the Pokemon game. You are now a Pokemon Trainer.')
-print('Would you like to catch some Pokemons?: Y or N')
+print(
+    f"Hey {player_1_name}! Welcome to the Pokemon game. "
+    f"You are now a Pokemon Trainer."
+)
+print("Would you like to catch some Pokemons?: Y or N")
 
 player_1 = Trainer(player_1_name)
 response = input().strip().lower()
 pok_1 = None
-if response in ['y', 'yes']:
-    print('Let\'s throw a Pokeball to catch your Pokemon! Sound good?')
+if response in ["y", "yes"]:
+    print("Let's throw a Pokeball to catch your Pokemon! Sound good?")
     input()
-    pokemon_1_type, pokemon_1_attributes = random.choice(list(pokemon_dict.items()))
-    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_1_attributes 
-    pok_1 = pokemon_instance = pokemon_1_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
-    player_1.throw_pokeball(pokemon_instance) 
-    print(f'You have caught: {pokemon_instance.name} \U0001F49A  Type: {pokemon_instance.pok_type}, Hitpoints: {pokemon_instance.hit_points}, Move: {pokemon_instance.move}, Damage: {pokemon_instance.attack_damage}, Strength: {pokemon_instance.strong_against}, Weakness: {pokemon_instance.weak_against}')
+    pokemon_1_type, pokemon_1_attributes = random.choice(
+        list(pokemon_dict.items())
+    )
+    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+        pokemon_1_attributes
+    )
+    pok_1 = pokemon_instance = pokemon_1_type(
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+    )
+    player_1.throw_pokeball(pokemon_instance)
+    print(
+        f"You have caught: {pokemon_instance.name} \U0001f49a "
+        f"Type: {pokemon_instance.pok_type}, "
+        f"Hitpoints: {pokemon_instance.hit_points}, "
+        f"Move: {pokemon_instance.move}, "
+        f"Damage: {pokemon_instance.attack_damage}, "
+        f"Strength: {pokemon_instance.strong_against}, "
+        f"Weakness: {pokemon_instance.weak_against}"
+    )
 else:
-    pokemon_1_type, pokemon_1_attributes = random.choice(list(pokemon_dict.items()))
-    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_1_attributes 
-    pok_1 = pokemon_instance = pokemon_1_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
-    player_1.throw_pokeball(pokemon_instance) 
-    print(f'No worries! For the purpose of this Game your Pokemon is {pokemon_instance.name} \U0001F49A  Type: {pokemon_instance.pok_type}, Hitpoints: {pokemon_instance.hit_points}, Move: {pokemon_instance.move}, Damage: {pokemon_instance.attack_damage}, Strength: {pokemon_instance.strong_against}, Weakness: {pokemon_instance.weak_against}')
+    pokemon_1_type, pokemon_1_attributes = random.choice(
+        list(pokemon_dict.items())
+    )
+    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+        pokemon_1_attributes
+    )
+    pok_1 = pokemon_instance = pokemon_1_type(
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+    )
+    player_1.throw_pokeball(pokemon_instance)
+    print(
+        f"No worries! "
+        f"For the purpose of this Game your Pokemon is "
+        f"{pokemon_instance.name}"
+        f" \U0001f49a "
+        f"Type: {pokemon_instance.pok_type}, "
+        f"Hitpoints: {pokemon_instance.hit_points}, "
+        f"Move: {pokemon_instance.move}, "
+        f"Damage: {pokemon_instance.attack_damage}, "
+        f"Strength: {pokemon_instance.strong_against}, "
+        f"Weakness: {pokemon_instance.weak_against}"
+    )
 
-### PLAYER 2 - POKEMON 2 ###
-     
-print('Please enter the name of player_2.')
+# PLAYER 2 - POKEMON 2 #
+
+print("Please enter the name of player_2.")
 player_2_name = input()
-print(f'Hey {player_2_name}! Welcome to the Pokemon game. You are now a Pokemon Trainer.')
-print('Would you like to catch some Pokemons?: Y or N')
+print(
+    f"Hey {player_2_name}! Welcome to the Pokemon game. "
+    f"You are now a Pokemon Trainer."
+)
+print("Would you like to catch some Pokemons?: Y or N")
 
 player_2 = Trainer(player_2_name)
 response = input().strip().lower()
 pok_2 = None
-if response in ['y', 'yes']:
-    print('Let\'s throw a Pokeball to catch your Pokemon! Sound good?')
+if response in ["y", "yes"]:
+    print("Let's throw a Pokeball to catch your Pokemon! Sound good?")
     input()
-    pokemon_2_type, pokemon_2_attributes = random.choice(list(pokemon_dict.items()))
-    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_2_attributes 
-    pok_2 = pokemon_instance = pokemon_2_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
-    player_2.throw_pokeball(pokemon_instance) 
+    pokemon_2_type, pokemon_2_attributes = random.choice(
+        list(pokemon_dict.items())
+    )
+    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+        pokemon_2_attributes
+    )
+    pok_2 = pokemon_instance = pokemon_2_type(
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+    )
+    player_2.throw_pokeball(pokemon_instance)
     if pok_2 == pok_1:
-        pokemon_2_type, pokemon_2_attributes = random.choice(list(pokemon_dict.items()))
-        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_2_attributes 
-        pok_2 = pokemon_instance = pokemon_2_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
+        pokemon_2_type, pokemon_2_attributes = random.choice(
+            list(pokemon_dict.items())
+        )
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+            pokemon_2_attributes
+        )
+        pok_2 = pokemon_instance = pokemon_2_type(
+            pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+        )
         player_2.throw_pokeball(pokemon_instance)
     else:
-        print(f'You have caught: {pokemon_instance.name} \U0001F49A Type: {pokemon_instance.pok_type}, Hitpoints: {pokemon_instance.hit_points}, Move: {pokemon_instance.move}, Damage: {pokemon_instance.attack_damage}, Strength: {pokemon_instance.strong_against}, Weakness: {pokemon_instance.weak_against}')
+        print(
+            f"You have caught: {pokemon_instance.name} \U0001f49a "
+            f"Type: {pokemon_instance.pok_type}, "
+            f"Hitpoints: {pokemon_instance.hit_points}, "
+            f"Move: {pokemon_instance.move}, "
+            f"Damage: {pokemon_instance.attack_damage}, "
+            f"Strength: {pokemon_instance.strong_against}, "
+            f"Weakness: {pokemon_instance.weak_against}"
+        )
 else:
-    pokemon_2_type, pokemon_2_attributes = random.choice(list(pokemon_dict.items()))
-    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_2_attributes 
-    pok_2 = pokemon_instance = pokemon_2_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
-    player_2.throw_pokeball(pokemon_instance) 
+    pokemon_2_type, pokemon_2_attributes = random.choice(
+        list(pokemon_dict.items())
+    )
+    pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+        pokemon_2_attributes
+    )
+    pok_2 = pokemon_instance = pokemon_2_type(
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+    )
+    player_2.throw_pokeball(pokemon_instance)
     if pok_2 == pok_1:
-        pokemon_2_type, pokemon_2_attributes = random.choice(list(pokemon_dict.items()))
-        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = pokemon_2_attributes 
-        pok_2 = pokemon_instance = pokemon_2_type(pokemon_name, hp, level, move, pokemon_type_str, strength, weakness)
+        pokemon_2_type, pokemon_2_attributes = random.choice(
+            list(pokemon_dict.items())
+        )
+        pokemon_name, hp, level, move, pokemon_type_str, strength, weakness = (
+            pokemon_2_attributes
+        )
+        pok_2 = pokemon_instance = pokemon_2_type(
+            pokemon_name, hp, level, move, pokemon_type_str, strength, weakness
+        )
         player_2.throw_pokeball(pokemon_instance)
     else:
-        print(f'No worries! For the purpose of this Game your Pokemon is {pokemon_instance.name} \U0001F49A Type: {pokemon_instance.pok_type}, Hitpoints: {pokemon_instance.hit_points}, Move: {pokemon_instance.move}, Damage: {pokemon_instance.attack_damage}, Strength: {pokemon_instance.strong_against}, Weakness: {pokemon_instance.weak_against}')
+        print(
+            f"No worries! "
+            f"For the purpose of this Game "
+            f"your Pokemon is {pokemon_instance.name} \U0001f49a "
+            f"Type: {pokemon_instance.pok_type}, "
+            f"Hitpoints: {pokemon_instance.hit_points}, "
+            f"Move: {pokemon_instance.move}, "
+            f"Damage: {pokemon_instance.attack_damage}, "
+            f"Strength: {pokemon_instance.strong_against}, "
+            f"Weakness: {pokemon_instance.weak_against}"
+        )
 
-print('It\'s time to Battle!')
+print("It's time to Battle!")
 battle = Battle(pok_1, pok_2)
 rounds = 10
 for i in range(10):
--- src/pokemon.py
+++ src/pokemon.py
@@ -1,5 +1,5 @@
 class Pokemon:
-    
+
     def __init__(self, name, hit_points, attack_damage, move):
         self.name = name
         self.hit_points = hit_points
@@ -7,7 +7,7 @@ class Pokemon:
         self.move = move
 
     def use_move(self):
-        return f'{self.name} used {self.move}'
+        return f"{self.name} used {self.move}"
 
     def take_damage(self, damage_points):
         self.hit_points = self.hit_points - damage_points
@@ -19,13 +19,23 @@ class Pokemon:
         else:
             return False
 
+
 class Fire(Pokemon):
-    def __init__(self,name, hit_points, attack_damage, move, pok_type, strong_against, weak_against):
+    def __init__(
+        self,
+        name,
+        hit_points,
+        attack_damage,
+        move,
+        pok_type,
+        strong_against,
+        weak_against,
+    ):
         super().__init__(name, hit_points, attack_damage, move)
         self.pok_type = pok_type
         self.strong_against = strong_against
         self.weak_against = weak_against
-        
+
     def get_multiplier(self, pokemon):
         self.pokemon = pokemon
         if self.strong_against == pokemon.pok_type:
@@ -34,14 +44,24 @@ class Fire(Pokemon):
             return 0.5
         else:
             return 1
+
 
 class Water(Pokemon):
-    def __init__(self,name, hit_points, attack_damage, move, pok_type, strong_against, weak_against):
+    def __init__(
+        self,
+        name,
+        hit_points,
+        attack_damage,
+        move,
+        pok_type,
+        strong_against,
+        weak_against,
+    ):
         super().__init__(name, hit_points, attack_damage, move)
         self.pok_type = pok_type
         self.strong_against = strong_against
         self.weak_against = weak_against
-        
+
     def get_multiplier(self, pokemon):
         self.pokemon = pokemon
         if self.strong_against == pokemon.pok_type:
@@ -50,9 +70,19 @@ class Water(Pokemon):
             return 0.5
         else:
             return 1
+
 
 class Grass(Pokemon):
-    def __init__(self,name, hit_points, attack_damage, move, pok_type, strong_against, weak_against):
+    def __init__(
+        self,
+        name,
+        hit_points,
+        attack_damage,
+        move,
+        pok_type,
+        strong_against,
+        weak_against,
+    ):
         super().__init__(name, hit_points, attack_damage, move)
         self.pok_type = pok_type
         self.strong_against = strong_against
@@ -66,14 +96,24 @@ class Grass(Pokemon):
             return 0.5
         else:
             return 1
+
 
 class Normal(Pokemon):
-    def __init__(self,name, hit_points, attack_damage, move, pok_type, strong_against, weak_against):
+    def __init__(
+        self,
+        name,
+        hit_points,
+        attack_damage,
+        move,
+        pok_type,
+        strong_against,
+        weak_against,
+    ):
         super().__init__(name, hit_points, attack_damage, move)
         self.pok_type = pok_type
         self.strong_against = strong_against
         self.weak_against = weak_against
-    
+
     def get_multiplier(self, pokemon):
         self.pokemon = pokemon
         if self.strong_against == pokemon.pok_type:
@@ -82,24 +122,26 @@ class Normal(Pokemon):
             return 0.5
         else:
             return 1
-        
+
+
 class Pokeball:
-        def __init__(self):
-            self.pokeball = None
+    def __init__(self):
+        self.pokeball = None
 
-        def catch(self, pokemon):
-            self.pokemon = pokemon
-            if self.is_empty():
-                self.pokeball = pokemon
-            else:
-                raise Exception('pokeball is full')
+    def catch(self, pokemon):
+        self.pokemon = pokemon
+        if self.is_empty():
+            self.pokeball = pokemon
+        else:
+            raise Exception("pokeball is full")
 
-        def is_empty(self):
-            return not self.pokeball    
-        
+    def is_empty(self):
+        return not self.pokeball
+
+
 class Trainer:
     def __init__(self, name):
-        self.name = name  
+        self.name = name
         self.belt = [Pokeball() for i in range(6)]
 
     def throw_pokeball(self, pokemon):
@@ -107,8 +149,9 @@ class Trainer:
             if pok.is_empty():
                 pok.catch(pokemon)
                 return pok
-        raise Exception('Belt is full')
-            
+        raise Exception("Belt is full")
+
+
 class Battle:
     def __init__(self, pokemon_1, pokemon_2):
         self.pokemon_1 = pokemon_1
@@ -117,25 +160,57 @@ class Battle:
 
     def take_turn(self):
         while self.pokemon_1.hit_points > 0 and self.pokemon_2.hit_points > 0:
-            attacker = self.pokemon_1 if self.whos_turn % 2 == 0 else self.pokemon_2
-            defender = self.pokemon_1 if self.whos_turn % 2 != 0 else self.pokemon_2
+            attacker = (
+                self.pokemon_1 if self.whos_turn % 2 == 0 else self.pokemon_2
+            )
+            defender = (
+                self.pokemon_1 if self.whos_turn % 2 != 0 else self.pokemon_2
+            )
             damage = attacker.get_multiplier(defender) + attacker.attack_damage
             defender.take_damage(damage)
             self.whos_turn += 1
             if self.pokemon_1.hit_points > 0 and self.pokemon_2.hit_points > 0:
-                print(f'Round {self.whos_turn}: {defender.name} has taken {damage} damage points, remaining hit points {defender.hit_points}')
-                return(f'Round {self.whos_turn}: {defender.name} has taken {damage} damage points, remaining hit points {defender.hit_points}')
+                print(
+                    f"Round {self.whos_turn}: {defender.name} "
+                    f"has taken {damage} damage points, "
+                    f"remaining hit points {defender.hit_points}"
+                )
+                return (
+                    f"Round {self.whos_turn}: {defender.name} "
+                    f"has taken {damage} damage points, "
+                    f"remaining hit points {defender.hit_points}"
+                )
             else:
-                print(f'Round {self.whos_turn}: {defender.name} has taken {damage} damage points, {defender.name} has fainted.')
-                return (f'Round {self.whos_turn}: {defender.name} has taken {damage} damage points, {defender.name} has fainted.')
-            
+                print(
+                    f"Round {self.whos_turn}: {defender.name} "
+                    f"has taken {damage} damage points, "
+                    f"{defender.name} has fainted."
+                )
+                return (
+                    f"Round {self.whos_turn}: {defender.name} "
+                    f"has taken {damage} damage points, {defender.name} "
+                    f"has fainted."
+                )
+
     def get_winner(self):
         if self.pokemon_1.hit_points < 0:
-            print(f'{self.pokemon_1.name} has fainted, winner is {self.pokemon_2.name}.')
-            return (f'{self.pokemon_1.name} has fainted, winner is {self.pokemon_2.name}.')
-        elif self.pokemon_2.hit_points < 0: 
-            print(f'{self.pokemon_2.name} has fainted, winner is {self.pokemon_1.name}.')
-            return (f'{self.pokemon_2.name} has fainted, winner is {self.pokemon_1.name}.')
+            print(
+                f"{self.pokemon_1.name} has fainted, "
+                f"winner is {self.pokemon_2.name}."
+            )
+            return (
+                f"{self.pokemon_1.name} has fainted, "
+                f"winner is {self.pokemon_2.name}."
+            )
+        elif self.pokemon_2.hit_points < 0:
+            print(
+                f"{self.pokemon_2.name} has fainted, "
+                f"winner is {self.pokemon_1.name}."
+            )
+            return (
+                f"{self.pokemon_2.name} has fainted, "
+                f"winner is {self.pokemon_1.name}."
+            )
         else:
-            print('Winner:', None)
-            return (None)
+            print("Winner:", None)
+            return None
