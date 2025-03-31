9252825e98de52898e78a1f1b72629bdf956199d
dghvici <daniela.ghinchevici@gmail.com>
2025-03-31 18:06:03 +0100

    extracting pokemon name, type and move from the api call, to be integrated in the game

--- api-calls.py
+++ api-calls.py
@@ -6,9 +6,13 @@ from pprint import pprint
 base_url = "https://pokeapi.co/api/v2/pokemon/"
 def get_pokemon():
     id = random.randint(0, 999)
-    url = f"{base_url}/{id}/"
-    response = requests.get(url)
-    data = response.json()
-    pprint(data)
+    if id:
+        pokemon_url = f"{base_url}/{id}/"
+        response = requests.get(pokemon_url)
+        data = response.json()
+        poke_name = data["name"]
+        poke_type = [type_info['type']['name'] for type_info in data['types']][0]
+        moves = [move_info['move']['name'] for move_info in data['moves']][0]
+        print(poke_name, poke_type, moves)
 
 get_pokemon()
